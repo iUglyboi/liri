3843bc2351a33b51110e60009e0807b308927e4e
iuglyboi28 <iuglyboi28@gmail.com>
2019-08-13 21:03:09 -0400

    did some stuff

--- homework/spotify.js
+++ homework/spotify.js
@@ -10,20 +10,31 @@ let spotify = new Spotify(keys.spotify);
 let whatItSays = require("./says.js")
 
 module.exports = function spotifySearch(userSearch) {
-    if (!userSearch)
-    spotify.search({type: "track, query: userSearch, limit: 1"}),
-    function (response){
-        if(!response){ 
-            let spotifyArray = data.tracks.items
-
-            for (i=0; i < spotifyArray; i++){
-                console.log("Song: " + data.tracks.items[i].name + " " +
-                "Artist: " + data.tracks.items[i].artist[0].name + " " +
-                "Album: " + data.tracks.items[i].album.name + " " +
-                "Preview Link: " + data.tracks.items[i].external_urls.spotify)
-        }}
-        else{
-            return console.log ("There was a error in your search")
-            }
+    spotify.search({type: "track, query:" + userSearch + ", limit: 1"}, function(err, data) {
+        if(data) {
+        console.log("Song: " + data.tracks.items[i].name + " " +
+      "Artist: " + data.tracks.items[i].artist[0].name + " " +
+      "Album: " + data.tracks.items[i].album.name + " " +
+      "Preview Link: " + data.tracks.items[i].external_urls.spotify)
+    }
+       else{
+        console.log('Error occurred: ' + err)
+        return
+    }})
+    //   else{
+    //         return console.log ("There was a error in your search")
+    //         }
         
-        }};
+    //     }};
+
+    // function (response){
+    //     if(!response){ 
+    //         let spotifyArray = data.tracks.items
+
+    //         for (i=0; i < spotifyArray; i++){
+    //             console.log("Song: " + data.tracks.items[i].name + " " +
+    //             "Artist: " + data.tracks.items[i].artist[0].name + " " +
+    //             "Album: " + data.tracks.items[i].album.name + " " +
+    //             "Preview Link: " + data.tracks.items[i].external_urls.spotify)
+        
+        
